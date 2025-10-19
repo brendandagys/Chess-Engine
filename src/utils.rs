b1374ef2af69bac22d5416b94e5eccf385fdb05b
Brendan Dagys <brendandagys@gmail.com>
2025-10-19 12:31:19 -0400

    Wording improvement

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,6 @@ use std::time::{SystemTime, UNIX_EPOCH};
 pub fn get_time() -> u64 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
+        .expect("Time went backward")
         .as_millis() as u64
 }
